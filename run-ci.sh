5ace4170c2886e2a0a4ef6c452012b6faf509334
Lup Yuen Lee <luppy@appkaki.com>
2024-10-18 18:08:54 +0800

    Clean up

--- run-ci.sh
+++ run-ci.sh
@@ -5,23 +5,25 @@
 set -x  ## Echo commands
 
 for (( ; ; )); do
-for board in \
-  arm-01 arm-02 arm-03 arm-04 \
-  arm-05 arm-06 arm-07 arm-08 \
-  arm-09 arm-10 arm-11 arm-12 \
-  arm-13 arm-14
-do
-sudo docker pull \
-  ghcr.io/apache/nuttx/apache-nuttx-ci-linux:latest
-sudo docker run -it \
-  ghcr.io/apache/nuttx/apache-nuttx-ci-linux:latest \
-  /bin/bash -c "
-  cd ;
-  pwd ;
-  git clone https://github.com/apache/nuttx ;
-  git clone https://github.com/apache/nuttx-apps apps ;
-  cd nuttx/tools/ci ;
-  ./cibuild.sh -c -A -N -R testlist/$board.dat ;
-"
-done
+  for board in \
+    arm-01 arm-02 arm-03 arm-04 \
+    arm-05 arm-06 arm-07 arm-08 \
+    arm-09 arm-10 arm-11 arm-12 \
+    arm-13 arm-14
+  do
+    sudo docker pull \
+      ghcr.io/apache/nuttx/apache-nuttx-ci-linux:latest
+    sudo docker run -it \
+      ghcr.io/apache/nuttx/apache-nuttx-ci-linux:latest \
+      /bin/bash -c "
+      cd ;
+      pwd ;
+      git clone https://github.com/apache/nuttx ;
+      git clone https://github.com/apache/nuttx-apps apps ;
+      pushd nuttx ; echo NuttX Source: https://github.com/apache/nuttx/tree/\$(git rev-parse HEAD) ; popd ;
+      pushd apps  ; echo NuttX Apps: https://github.com/apache/nuttx-apps/tree/\$(git rev-parse HEAD) ; popd ;
+      cd nuttx/tools/ci ;
+      ./cibuild.sh -c -A -N -R testlist/$board.dat ;
+    "
+  done
 done
