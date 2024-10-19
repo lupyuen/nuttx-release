e6c5c8e5efab6e16560b9224035cc843edd047ce
Lup Yuen Lee <luppy@appkaki.com>
2024-10-19 16:06:36 +0700

    Upload the CI Log

--- run-ci.sh
+++ run-ci.sh
@@ -4,35 +4,77 @@
 # set -e  ## Exit when any command fails
 set -x  ## Echo commands
 
+device=ci
+
+## Get the Script Directory
+script_path="${BASH_SOURCE}"
+script_dir="$(cd -P "$(dirname -- "${script_path}")" >/dev/null 2>&1 && pwd)"
+log_file=/tmp/release-$device.log
+
+## Get the `script` option
+if [ "`uname`" == "Linux" ]; then
+  script_option=-c
+else
+  script_option=
+fi
+
+## Run the job
+function run_job {
+  local job=$1
+  pushd /tmp
+  script $log_file \
+    $script_option \
+    "$script_dir/run-job.sh $job"
+  popd
+}
+
+## Strip the control chars
+function clean_log {
+  tmp_file=/tmp/release-tmp.log
+  cat $log_file \
+    | tr -d '\r' \
+    | tr -d '\r' \
+    | sed 's/\x08/ /g' \
+    | sed 's/\x1B(B//g' \
+    | sed 's/\x1B\[K//g' \
+    | sed 's/\x1B[<=>]//g' \
+    | sed 's/\x1B\[[0-9:;<=>?]*[!]*[A-Za-z]//g' \
+    | sed 's/\x1B[@A-Z\\\]^_]\|\x1B\[[0-9:;<=>?]*[-!"#$%&'"'"'()*+,.\/]*[][\\@A-Z^_`a-z{|}~]//g' \
+    >$tmp_file
+  mv $tmp_file $log_file
+  echo ----- "Done! $log_file"
+}
+
+## Upload to GitHub Gist
+function upload_log {
+  local job=$1
+  local nuttx_hash=$2
+  local apps_hash=$3
+  cat $log_file | \
+    gh gist create \
+    --public \
+    --desc "[$job] CI Log for nuttx @ $nuttx_hash / nuttx-apps @ $apps_hash" \
+    --filename "ci-$job.log"
+}
+
+## Repeat forever for All CI Jobs
 for (( ; ; )); do
-  for board in \
+  for job in \
     arm-01 arm-02 arm-03 arm-04 \
     arm-05 arm-06 arm-07 arm-08 \
     arm-09 arm-10 arm-11 arm-12 \
     arm-13 arm-14
   do
-    ## Show the System Info
-    neofetch
-    sleep 10
+    ## Run the CI Job
+    run_job $job
+    clean_log
 
-    ## Download the Docker Image
-    sudo docker pull \
-      ghcr.io/apache/nuttx/apache-nuttx-ci-linux:latest
-    sleep 10
+    ## Get the hashes for NuttX and Apps
+    nuttx_hash=$(grep --only-matching -E "nuttx/tree/[0-9a-z]+" $log_file | grep --only-matching -E "[0-9a-z]+$")
+    apps_hash=$(grep --only-matching -E "nuttx-apps/tree/[0-9a-z]+" $log_file | grep --only-matching -E "[0-9a-z]+$")
 
-    ## Run the CI in Docker Container
-    sudo docker run -it \
-      ghcr.io/apache/nuttx/apache-nuttx-ci-linux:latest \
-      /bin/bash -c "
-      cd ;
-      pwd ;
-      git clone https://github.com/apache/nuttx ;
-      git clone https://github.com/apache/nuttx-apps apps ;
-      pushd nuttx ; echo NuttX Source: https://github.com/apache/nuttx/tree/\$(git rev-parse HEAD) ; popd ;
-      pushd apps  ; echo NuttX Apps: https://github.com/apache/nuttx-apps/tree/\$(git rev-parse HEAD) ; popd ;
-      sleep 10 ;
-      cd nuttx/tools/ci ;
-      ./cibuild.sh -c -A -N -R testlist/$board.dat ;
-    "
+    ## Upload the log
+    upload_log $job $nuttx_hash $apps_hash
+    sleep 10
   done
 done
---
+++ run-job.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+## Run a NuttX CI Job with Docker
+
+# set -e  ## Exit when any command fails
+set -x  ## Echo commands
+
+# Parameter is CI Job, like "arm-01"
+job=$1
+
+## Show the System Info
+neofetch
+sleep 10
+
+## Download the Docker Image
+sudo docker pull \
+  ghcr.io/apache/nuttx/apache-nuttx-ci-linux:latest
+sleep 10
+
+## Run the CI in Docker Container
+sudo docker run -it \
+  ghcr.io/apache/nuttx/apache-nuttx-ci-linux:latest \
+  /bin/bash -c "
+  cd ;
+  pwd ;
+  git clone https://github.com/apache/nuttx ;
+  git clone https://github.com/apache/nuttx-apps apps ;
+  pushd nuttx ; echo NuttX Source: https://github.com/apache/nuttx/tree/\$(git rev-parse HEAD) ; popd ;
+  pushd apps  ; echo NuttX Apps: https://github.com/apache/nuttx-apps/tree/\$(git rev-parse HEAD) ; popd ;
+  sleep 10 ;
+  cd nuttx/tools/ci ;
+  ./cibuild.sh -c -A -N -R testlist/$job.dat ;
+"
