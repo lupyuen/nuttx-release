b2d69f4333c5ea6d3d968b7847daa7c409f7952c
Lup Yuen Lee <luppy@appkaki.com>
2024-10-26 21:49:52 +0700

    Fix messages

--- run-ci.sh
+++ run-ci.sh
@@ -50,7 +50,7 @@ function find_messages {
   local tmp_file=/tmp/release-tmp.log
   local msg_file=/tmp/release-msg.log
   local pattern='^(.*):(\d+):(\d+):\s+(warning|fatal error|error):\s+(.*)$'
-  grep "*****" $log_file \
+  grep '^\*\*\*\*\*' $log_file \
     > $msg_file
   grep -P "$pattern" $log_file \
     | uniq \
