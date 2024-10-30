262cbda183e12a11e7fdede842863c5ce14fe1a1
Lup Yuen Lee <luppy@appkaki.com>
2024-10-30 20:16:10 +0700

    Add sim-03

--- run-ci.sh
+++ run-ci.sh
@@ -74,11 +74,12 @@ function upload_log {
 ## Repeat forever for All CI Jobs
 for (( ; ; )); do
   for job in \
-    risc-v-02 risc-v-03 \
     arm-01 arm-02 arm-03 arm-04 \
     arm-05 arm-06 arm-07 arm-08 \
     arm-09 arm-10 arm-11 arm-12 \
-    arm-13 arm-14
+    arm-13 arm-14 \
+    risc-v-02 risc-v-03 \
+    sim-03
   do
     ## Run the CI Job and find errors / warnings
     run_job $job
